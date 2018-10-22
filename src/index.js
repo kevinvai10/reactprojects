862dd381fee15dce199f403d4c958676c46b9684
kevin <kevin@simbo.mx>
2018-10-21 22:57:38 -0700

    Color and state management

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import CardList from './CardList';
+import App from './App';
 import 'tachyons';
 import {robots} from './robots';
 
-ReactDOM.render(<CardList robots ={robots}/>, document.getElementById('root'));
+ReactDOM.render(<App robots ={robots}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
